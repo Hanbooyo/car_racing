4d31ac67591f44d961c3acec9bb8cccd4d9de5d1
Hanbooyo <qnfudk@gmail.com>
2023-11-01 19:37:38 +0900

    StringTest 샘플

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 public class StringTest {
     @Test
@@ -11,4 +12,30 @@ public class StringTest {
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2"); // 정확한 순서까지
     }
+
+    @Test
+    void subString() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항3 : charAt Method")
+    void charAt() {
+
+        String abc = "abc";
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    abc.charAt(4);
+                }).withMessageMatching("Index: "+4+", Size: "+abc.length());
+
+        assertThatThrownBy(() -> {
+            abc.charAt(2);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Index: 2, Size: 2");
+    }
+
+
+
 }
